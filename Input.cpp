dce1289ee1b4ab0f729eb8fcd75630cdffdafe59
Emily <anny.baquerizolema@heig-vd.ch>
2022-11-16 23:23:34 +0100

    Input: add the surcharge of the three methods

--- Input.cpp
+++ Input.cpp
@@ -8,43 +8,103 @@
 using namespace std;
 
 int Input::askUserInt(int min) {
-    cout << "Enter a value: ";
     int value;
-    while (!(cin >> value)) {
-        cout << "The value you entered is not an integer, try again: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n ');
+    while (true) {
+        cout << "Enter a value greater than " << min << ": ";
+        while (!(cin >> value)) {
+            cout << "The value you entered is not an integer, try again: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n   ');
+        }
+        if (value >= min) {
+            break;
+        }
+        cout << "The value is smaller than " << min << endl;
     }
-};
+    return value;
+}
 
 int Input::askUserInt(int min, int max) {
-
-};
+    int value;
+    while (true) {
+        cout << "Enter a value between " << min << " and " << max << ": ";
+        while (!(cin >> value)) {
+            cout << "The value you entered is not an integer, try again: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n   ');
+        }
+        if (value >= min && value <= max) {
+            break;
+        }
+        cout << "The value you entered is not in the range." << endl;
+    }
+    return value;
+}
 
 float Input::askUserFloat(float min) {
-    cout << "Enter a value: ";
     float value;
-    while (!(cin >> value)) {
-        cout << "The value you entered is invalid, try again: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n ');
+    while (true) {
+        cout << "Enter a value greater than " << min << ": ";
+        while (!(cin >> value)) {
+            cout << "The value you entered is invalid, try again: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n   ');
+        }
+        if (value >= min) {
+            break;
+        }
+        cout << "The value is smaller than " << min << endl;
     }
-};
+    return value;
+}
 
 float Input::askUserFloat(float min, float max) {
-
-};
+    float value;
+    while (true) {
+        cout << "Enter a value between " << min << " and " << max << ": ";
+        while (!(cin >> value)) {
+            cout << "The value you entered is invalid, try again: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n   ');
+        }
+        if (value >= min && value <= max) {
+            break;
+        }
+        cout << "The value you entered is not in the range." << endl;
+    }
+    return value;
+}
 
 double Input::askUserDouble(double min) {
-    cout << "Enter a value: ";
     double value;
-    while(!(cin >> value)){
-        cout << "The value you entered is invalid, try again: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n ');
+    while (true) {
+        cout << "Enter a value greater than " << min << ": ";
+        while (!(cin >> value)) {
+            cout << "The value you entered is invalid, try again: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n   ');
+        }
+        if (value >= min) {
+            break;
+        }
+        cout << "The value is smaller than " << min << endl;
     }
-};
+    return value;
+}
 
 double Input::askUserDouble(double min, double max) {
-
-};
+    double value;
+    while (true) {
+        cout << "Enter a value between " << min << " and " << max << ": ";
+        while (!(cin >> value)) {
+            cout << "The value you entered is invalid, try again: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n   ');
+        }
+        if (value >= min && value <= max) {
+            break;
+        }
+        cout << "The value you entered is not in the range." << endl;
+    }
+    return value;
+}
--- Input.h
+++ Input.h
@@ -4,6 +4,7 @@
 
 #ifndef CONNECT4_INPUT_H
 #define CONNECT4_INPUT_H
+#include <string>
 
 class Input {
 public:
@@ -15,6 +16,11 @@ public:
 
     static double askUserDouble(double min);
     static double askUserDouble(double min, double max);
+    /*
+private: possibility of having errorMessage in a variable
+    const std::string errorOfTypeInput =  "The value you entered is not correct.";
+    const std::string errorInputNotInRange = "The value you entered is not in the range.";
+     */
 };
 
 #endif //CONNECT4_INPUT_H
