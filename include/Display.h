f8cc67f86466c5c2111e92a2a81d85e7c2048baf
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-11-17 13:46:15 +0100

    Merge 2 projects in Display.cpp

--- include/Display.h
+++ include/Display.h
@@ -1,6 +1,9 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
+#include <iomanip>
+#include <map>
 #include <string>
+#include <vector>
 
 enum class Color { RED,
                    GREEN,
@@ -15,5 +18,6 @@ public:
     static void clearScreen();
     static std::string getColoredText(const std::string &text, Color color);
     static void printUTF8Char(int code);
+    static void print2DArray(const std::vector<std::vector<int>> &array, std::map<int, char> charmap, std::map<int, std::string> colormap, bool borders, std::string borderColor);
 };
 #endif
--- src/Display.cpp
+++ src/Display.cpp
@@ -1,17 +1,26 @@
 #include "../include/Display.h"
+#include <iomanip>
 #include <iostream>
+#include <map>
 #include <string>
+#include <vector>
 #ifdef _WIN32
 #include <windows.h>
 #endif
 using namespace std;
+
+const string COLOR_RESET = "\x1b[0;0m";
+const string COLOR_RED = "\x1b[38;2;255;0;0m";
+const string COLOR_YELLOW = "\x1b[38;2;255;255;0m";
+const string COLOR_BLUE = "\x1b[38;2;85;85;255m";
 
 
 void Display::clearScreen() {
 #ifdef _WIN32
     system("cls");
 #else
-    system("clear");
+    cout << "\033[2J;"
+         << "\033[1;1H";
 #endif
 }
 
@@ -29,7 +38,22 @@ void Display::printUTF8Char(int code) {
     // system(("chcp 65001 "s + to_string(CP_UTF8)).c_str());
     SetConsoleOutputCP(CP_UTF8);
 #endif
-
-    printf("\\u%04x", code);
+    string test = "\\u" + to_string(code);
+    // printf("\\u%04x", code);
+    cout << test;
     // cout << to_wstring("\\u" + to_string(code));
+}
+
+
+void Display::print2DArray(const vector<vector<int>> &array, map<int, char> charmap, map<int, string> colormap, bool borders, string borderColor) {
+    for (int y = 0; y < array[0].size(); ++y) {
+        for (int x = 0; x < array.size(); ++x) {
+            cout << borderColor << setw(2) << (borders ? "|" : "")
+                 << colormap.at(array[x][y]) << setw(2) << charmap.at(array[x][y]);
+        }
+        if (borders)
+            cout << borderColor << setw(2) << "|";
+        cout << endl;
+    }
+    cout << COLOR_RESET;
 }
